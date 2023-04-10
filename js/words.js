39aa8e2b9eec2d0cf3975e22211390865ffe239d
Marcelo Moreira <96429088+Marcelo-devweb@users.noreply.github.com>
2023-04-09 21:24:01 -0300

    Add files via upload

---
+++ js/words.js
@@ -0,0 +1,194 @@
+const wordList = [
+{
+         word: "python",
+         hint: "linguagem de programação"
+     },
+     {
+         word: "guitarra",
+         hint: "instrumento musical"
+     },
+     {
+         word: "objetivo",
+         hint: "um propósito ou intenção"
+     },
+     {
+         word: "venus",
+         hint: "planeta do nosso sistema solar"
+     },
+     {
+         word: "ouro",
+         hint: "um metal precioso amarelo"
+     },
+     {
+         word: "ebay",
+         hint: "site de compras online"
+     },
+     {
+         word: "golang",
+         hint: "linguagem de programação"
+     },
+     {
+         word: "codificação",
+         hint: "relacionado à programação"
+     },
+     {
+         word: "matriz",
+         hint: "filme de ficção científica"
+     },
+     {
+         word: "bugs",
+         hint: "relacionado à programação"
+     },
+     {
+         word: "avatar",
+         hint: "filme épico de ficção científica"
+     },
+     {
+         word: "gif",
+         hint: "um formato de arquivo para imagem"
+     },
+     {
+         word: "mental",
+         hint: "relacionado com a mente"
+     },
+     {
+         word: "mapa",
+         hint: "diagrama representa uma área"
+     },
+     {
+         word: "ilha",
+         hint: "terra cercada por água"
+     },
+     {
+         word: "hóquei",
+         hint: "um famoso jogo ao ar livre"
+     },
+     {
+         word: "xadrez",
+         hint: "relacionado a um jogo interno"
+     },
+     {
+         word: "viber",
+         hint: "um aplicativo de mídia social"
+     },
+     {
+         word: "github",
+         hint: "plataforma de hospedagem de código"
+     },
+     {
+         word: "png",
+         hint: "um formato de arquivo de imagem"
+     },
+     {
+         word: "prata",
+         hint: "precioso metal branco acinzentado"
+     },
+     {
+         word: "móvel",
+         hint: "um dispositivo eletrônico"
+     },
+     {
+         word: "gpu",
+         hint: "componente do computador"
+     },
+     {
+         word: "java",
+         hint: "linguagem de programação"
+     },
+     {
+         word: "google",
+         hint: "motor de busca famoso"
+     },
+     {
+         word: "veneza",
+         hint: "famosa cidade das águas"
+     },
+     {
+         word: "excel",
+         hint: "produto microsoft para windows"
+     },
+     {
+         word: "mysql",
+         hint: "um sistema de banco de dados relacional"
+     },
+     {
+         word: "nepal",
+         hint: "nome do país em desenvolvimento"
+     },
+     {
+         word: "flauta",
+         hint: "um instrumento musical"
+     },
+     {
+         word: "cripto",
+         hint: "relacionado a criptomoeda"
+     },
+     {
+         word: "tesla",
+         hint: "unidade de densidade de fluxo magnético"
+     },
+     {
+         word: "marte",
+         hint: "planeta do nosso sistema solar"
+     },
+     {
+         word: "procurador",
+         hint: "relacionado ao aplicativo do servidor"
+     },
+     {
+         word: "e-mail",
+         hint: "relacionado à troca de mensagem"
+     },
+     {
+         word: "html",
+         hint: "linguagem de marcação para a web"
+     },
+     {
+         word: "ar",
+         hint: "relacionado a um gás"
+     },
+     {
+         word: "idéia",
+         hint: "um pensamento ou sugestão"
+     },
+     {
+         word: "servidor",
+         hint: "relacionado ao computador ou sistema"
+     },
+     {
+         word: "svg",
+         hint: "um formato de imagem vetorial"
+     },
+     {
+         word: "jpeg",
+         hint: "um formato de arquivo de imagem"
+     },
+     {
+         word: "chave",
+         hint: "pequeno pedaço de metal"
+     },
+     {
+         word: "Egito",
+         hint: "um nome de país"
+     },
+     {
+         word: "coringa",
+         hint: "filme de suspense psicológico"
+     },
+     {
+         word: "dubai",
+         hint: "nome do país desenvolvido"
+     },
+     {
+         word: "foto",
+         hint: "representação de pessoa ou cena"
+     },
+     {
+         word: "nilo",
+         hint: "maior rio do mundo"
+     },
+     {
+         word: "chuva",
+         hint: "relacionado a uma água"
+     },
+ ]
